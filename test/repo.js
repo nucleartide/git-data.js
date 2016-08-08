a9caf858f9f508d5a1225fed1f5c6092282783e6
Jason Tu <jasontu4@gmail.com>
2016-08-08 12:21:47 -0400

    implement and test .deleteFile(path)

--- test/repo.js
+++ test/repo.js
@@ -5,6 +5,7 @@ const Repo = require('../lib/repo')
 const assert = require('assert')
 const co = require('co')
 const GitHub = require('../lib/github')
+const clone = json => JSON.parse(JSON.stringify(json))
 
 const token = process.env.TOKEN
 if (!token) throw new Error('Must set TOKEN environment variable.')
@@ -23,8 +24,23 @@ describe('.detectFileType(path)', function() {
   })
 })
 
-describe('.deleteFile(path)', function() {
-  it('should work', function() {
+describe('._treeRes()', function() {
+  it('should return the cache if it exists', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+    r._treeCache = 'something'
+
+    return co(function*(){
+      const tree = yield r._treeRes()
+      assert.equal(tree, 'something')
+    })
+  })
+
+  it("should fetch the cache if it doesn't exist", function() {
     const g = new GitHub({ token })
     const r = g.repo({
       owner: 'nucleartide',
@@ -33,7 +49,135 @@ describe('.deleteFile(path)', function() {
     })
 
     return co(function*(){
-      yield r.deleteFile('test/shared.js')
+      const tree = yield r._treeRes()
+      assert('sha' in tree)
+      assert('url' in tree)
+      assert('tree' in tree)
+      assert('truncated' in tree)
+    })
+  })
+})
+
+describe('.deleteFile(path)', function() {
+  it('should throw an error if the path is empty', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    return co(function*(){
+      try {
+        yield r.deleteFile('')
+        throw new Error('Empty path should have thrown error.')
+      } catch (err) {
+        assert.equal(err.message, 'Path is empty.')
+      }
+    })
+  })
+
+  it('should remove the file from cache and destroy', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    const b = new Blob
+    r._blobCache['testCase'] = b
+    assert.equal(Object.keys(r._blobCache).length, 1)
+
+    return co(function*(){
+      yield r.deleteFile('testCase')
+      assert.equal(Object.keys(r._blobCache).length, 0)
+      assert(b.isDestroyed)
+    })
+  })
+
+  it('should throw an error if the file was not found', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    return co(function*(){
+      try {
+        yield r.deleteFile('not a file lollololol')
+        throw new Error('Should have thrown error')
+      } catch (err) {
+        assert.equal(err.message, 'File was not found.')
+      }
+    })
+  })
+
+  it('should throw an error if file is a directory', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    return co(function*(){
+      try {
+        yield r.deleteFile('test')
+        throw new Error('Should have thrown error')
+      } catch (err) {
+        assert.equal(err.message, 'File was not found.')
+      }
+    })
+  })
+
+  it('should remove a file in root dir from the tree cache', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    return co(function*(){
+      const initialCache = clone(yield r._treeRes())
+      yield r.deleteFile('index.js')
+      const updatedCache = yield r._treeRes()
+
+      const fileToDelete = updatedCache.tree.find(i => i.path === 'index.js')
+      assert(!fileToDelete, 'file was deleted')
+
+      assert.notEqual(updatedCache.sha, initialCache.sha, 'sha was updated')
+      assert.notEqual(updatedCache.url, initialCache.url, 'url was updated')
+    })
+  })
+
+  it('should remove a file in a nested dir from the tree cache', function() {
+    const g = new GitHub({ token })
+    const r = g.repo({
+      owner: 'nucleartide',
+      repo: 'git-data.js',
+      branch: 'master',
+    })
+
+    return co(function*(){
+      const initialCache = clone(yield r._treeRes())
+      yield r.deleteFile('test/github.js')
+      const updatedCache = yield r._treeRes()
+
+      const file = updatedCache.tree.find(i => i.path === 'test/github.js')
+      assert(!file, 'file was deleted')
+
+      assert.notEqual(updatedCache.sha, initialCache.sha,
+                      'root sha was updated')
+      assert.notEqual(updatedCache.url, initialCache.url,
+                      'root url was updated')
+
+      const testTree1 = initialCache.tree.find(i => i.path === 'test')
+      const testTree2 = updatedCache.tree.find(i => i.path === 'test')
+      assert.notEqual(testTree1.sha, testTree2.sha, 'test tree sha was updated')
+      assert.notEqual(testTree1.url, testTree2.url, 'test tree url was updated')
     })
   })
 })
--- test/util.js
+++ test/util.js
@@ -1,5 +1,13 @@
 
-const { merge, startsWith, endsWith, isDirectChild } = require('../lib/util')
+const {
+  merge,
+  startsWith,
+  endsWith,
+  isDirectChild,
+  basename,
+  dirname,
+  removeElement,
+} = require('../lib/util')
 const assert = require('assert')
 
 describe('.merge(a, b)', function() {
@@ -28,6 +36,26 @@ describe('.endsWith(str, end)', function() {
   })
 })
 
+describe('.basename(str)', function() {
+  it('works', function() {
+    assert.equal(basename('a/b/c/d'), 'd')
+    assert.equal(basename('a/b/c'), 'c')
+    assert.equal(basename('a/b'), 'b')
+    assert.equal(basename('a'), 'a')
+    assert.equal(basename(''), '')
+  })
+})
+
+describe('.dirname(str)', function() {
+  it('works', function() {
+    assert.equal(dirname('a/b/c/d'), 'a/b/c')
+    assert.equal(dirname('a/b/c'), 'a/b')
+    assert.equal(dirname('a/b'), 'a')
+    assert.equal(dirname('a'), '')
+    assert.equal(dirname(''), '')
+  })
+})
+
 describe('.isDirectChild(p1, p2)', function() {
   it('works', function() {
     assert(isDirectChild('test', 'test/shared.js'))
@@ -35,6 +63,21 @@ describe('.isDirectChild(p1, p2)', function() {
     assert(isDirectChild('', 'something.js'))
     assert(isDirectChild('', 'test.txt'))
     assert(isDirectChild('', 'test'))
+    assert(!isDirectChild('', 'test/something.js'))
+  })
+})
+
+describe('.removeElement(arr, cb)', function() {
+  it('should remove the element from the array', function() {
+    const arr = [{a:'hi'},{b:'hi'},{c:'hi'}]
+    const cb = o => 'a' in o
+    removeElement(arr, cb)
+    assert.deepEqual(arr, [{b:'hi'},{c:'hi'}])
+  })
+
+  it('should return with the removed element', function() {
+    const result = removeElement([1, 2, 3, 4], e => e === 2)
+    assert.equal(result, 2)
   })
 })
 
